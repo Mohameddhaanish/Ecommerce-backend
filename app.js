beec431a9a452d6e4a442551a43b13d446d69b0f
Mohameddhaanish <thufaledhanish@gmail.com>
2023-07-06 17:54:37 +0530

    cors changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const morgan = require("morgan");
+const cors = require("cors");
 const dbConnection = require("./config/db");
 require("dotenv").config();
 dbConnection();
